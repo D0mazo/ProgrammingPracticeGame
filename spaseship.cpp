cb2cee6f7a60300f1b328c4a7d850367f1dd20a5
D0mazo <dominykas.simanavicius@proton.me>
2025-02-23 00:43:31 +0200

    creation of spaseship

---
+++ spaseship.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Domin on 2.23.2025.
+//
+
+#include "spaseship.h"
---
+++ spaseship.h
@@ -0,0 +1,17 @@
+//
+// Created by Domin on 2.23.2025.
+//
+
+#ifndef SPASESHIP_H
+#define SPASESHIP_H
+
+
+
+class spaseship {
+
+
+};
+
+
+
+#endif //SPASESHIP_H
