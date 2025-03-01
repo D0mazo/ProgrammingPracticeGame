14865bb845813b070378175e68378d250abb25ae
D0mazo <dominykas.simanavicius@proton.me>
2025-03-01 11:06:38 +0200

    SpaceShip

--- spaseship.h
+++ spaseship.h
@@ -1,17 +1,39 @@
-//
-// Created by Domin on 2.23.2025.
-//
+// Created by Domin on 2.23.2025 (updated 3.01.2025)
+// File: spaceship.h
 
-#ifndef SPASESHIP_H
-#define SPASESHIP_H
+#ifndef SPACESHIP_H
+#define SPACESHIP_H
+
+#include <string>
+#include <iostream>
+
+class Spaceship {
+private:
+    std::string name;
+    int lives;
+    double shotPower;
+    int speed;
+
+public:
+
+    Spaceship(std::string shipName = "Unnamed Ship",
+              int lifeCount = 5,
+              double power = 1000.0,
+              int spd = 100) {
+        name = shipName;
+        lives = lifeCount;
+        shotPower = power;
+        speed = spd;
+    }
 
 
-
-class spaseship {
-
-
+    void describe() const {
+        std::cout << "Spaceship Details:" << std::endl;
+        std::cout << "Name: " << name << std::endl;
+        std::cout << "Lives: " << lives << std::endl;
+        std::cout << "Shot Power: " << shotPower << " units" << std::endl;
+        std::cout << "Speed: " << speed << " LY/h" << std::endl;
+    }
 };
 
-
-
-#endif //SPASESHIP_H
+#endif // SPACESHIP_H
